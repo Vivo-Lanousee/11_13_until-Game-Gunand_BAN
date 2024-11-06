60ef019416934065aef723a28b80f7ba113e7273
teacher <test@test.text>
2024-11-06 17:15:32 +0900

    フォント

--- GunandBAN/Assets/MainGame/Instance_Object/Object_User_and_Text/User_and_Text_Script.cs
+++ GunandBAN/Assets/MainGame/Instance_Object/Object_User_and_Text/User_and_Text_Script.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 /// <summary>
@@ -7,11 +8,15 @@ using UnityEngine;
 /// </summary>
 public class User_and_Text_Script : MonoBehaviour
 {
+    [Header("ユーザーのコメント")]
+    public TextMeshProUGUI CommentText;
+
+
     /// <summary>
     /// UserProfile結構悩ましいというか専用にスクリプト作ってもいいかも
     /// </summary>
     public GameObject UserProfile;
-    public GameObject TextProfile;
+
 
 
     // Start is called before the first frame update
