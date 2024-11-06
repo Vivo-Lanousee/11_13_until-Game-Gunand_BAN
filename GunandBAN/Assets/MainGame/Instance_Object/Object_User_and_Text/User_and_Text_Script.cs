e45d9e3d77753bbf75550333749a9feb6711b728
Amary_Lisu <ktc23a32a0003@edu.kyoto-tech.ac.jp>
2024-11-06 10:41:14 +0900

    各種UI等スクリプトの調整開始

---
+++ GunandBAN/Assets/MainGame/Instance_Object/Object_User_and_Text/User_and_Text_Script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// コメント欄のUserText用
+/// </summary>
+public class User_and_Text_Script : MonoBehaviour
+{
+    /// <summary>
+    /// UserProfile結構悩ましいというか専用にスクリプト作ってもいいかも
+    /// </summary>
+    public GameObject UserProfile;
+    public GameObject TextProfile;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
